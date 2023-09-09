ab621529b9e2740f1c7fd42eb2708afb64d816b4
hangj <guijie.han@gmail.com>
2023-09-09 21:46:22 +0800

    Add url for ip fetch

--- dnspoddns/src/utils.rs
+++ dnspoddns/src/utils.rs
@@ -4,6 +4,7 @@ use anyhow::{anyhow, Result};
 /// https://4.ipw.cn => 192.168.1.1
 /// https://test.ipw.cn => 192.168.1.1
 /// https://ipinfo.io/ip => 192.168.1.1
+/// https://myip.biturl.top => 192.168.1.1
 /// https://ipecho.net/plain => 192.168.1.1 (如果有 IPv6 地址的话, 则返回 IPv6)
 /// https://httpbin.org/ip => {"origin": "192.168.1.1"}
 /// http://6.ipw.cn => 有 IPv6 地址则返回, 否则出错
@@ -12,6 +13,7 @@ pub fn get_public_ip() -> Result<String> {
         "https://4.ipw.cn",
         "https://test.ipw.cn",
         "https://ipinfo.io/ip",
+        "https://myip.biturl.top",
     ];
 
     let i = rand::random::<usize>() % URLS.len();
